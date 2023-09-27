1840c1d7c454efe5be62cb314c852fbf603c4406
MatiCBL <brzoza.mathew@gmail.com>
2023-09-27 22:01:02 +0100

    'added-logic-for-AI-search-creating-embeddings-questions'

--- utils/ai.ts
+++ utils/ai.ts
@@ -1,7 +1,11 @@
+import { loadQARefineChain } from "langchain/chains";
+import { Document } from "langchain/document";
 import { OpenAI } from "langchain/llms/openai";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { StructuredOutputParser } from "langchain/output_parsers";
-import z from "zod";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
 import { PromptTemplate } from "langchain/prompts";
+import z from "zod";
 
 const parser = StructuredOutputParser.fromZodSchema(
   z.object({
@@ -50,4 +54,25 @@ export const analyze = async (content) => {
   } catch (error) {
     console.log(error);
   }
+};
+
+const qa = async (question, entries) => {
+  const docs = entries.map((entry) => {
+    return new Document({
+      pageContent: entry.content,
+      metadata: { id: entry.id, createdAt: entry.createdAt },
+    });
+  });
+
+  const model = new OpenAI({ temperature: 0, modelName: "gpt-3.5-turbo" });
+  const chain = loadQARefineChain(model);
+  const embeddings = new OpenAIEmbeddings();
+  const store = await MemoryVectorStore.fromDocuments(docs, embeddings);
+  const relevantDocs = await store.similaritySearch(question);
+  const res = await chain.call({
+    input_documents: relevantDocs,
+    question,
+  });
+
+  return res.output_text;
 };
